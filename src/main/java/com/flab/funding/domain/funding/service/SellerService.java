491a7590824c48f2955a27ddc7301ec782b9084e
deveun <bellar9311@gmail.com>
2022-01-16 21:26:59 +0900

    seller 기능 추가 (펀딩 삭제 , 펀딩 리스트 조회, 펀딩 상세내역 조회)

--- src/main/java/com/flab/funding/domain/funding/service/SellerService.java
+++ src/main/java/com/flab/funding/domain/funding/service/SellerService.java
@@ -1,9 +1,11 @@
 package com.flab.funding.domain.funding.service;
 
+import com.flab.funding.domain.funding.exception.NoFundingExistException;
 import com.flab.funding.domain.funding.model.FundingInfo;
 import com.flab.funding.domain.funding.repository.FundingJpaRepository;
-import com.flab.funding.domain.funding.repository.FundingMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,10 +16,24 @@ public class SellerService {
 
     private final FundingJpaRepository fundingJpaRepo;
 
-    public Optional<FundingInfo> addFunding(FundingInfo fundingInfo) {
+    public FundingInfo addFunding(FundingInfo fundingInfo) {
+        return fundingJpaRepo.save(fundingInfo);
+    }
 
-        fundingJpaRepo.save(fundingInfo);
+    public void deleteFunding(Long id) {
+        fundingJpaRepo.deleteById(id);
+    }
 
-        return Optional.of(fundingInfo);
+    public Page<FundingInfo> getFundingList(Long id, Pageable pageReq) {
+        return fundingJpaRepo.findBySeller_Id(id, pageReq);
+    }
+
+    public FundingInfo getFundingDetail(Long id) {
+
+        Optional<FundingInfo> fundingInfo = fundingJpaRepo.findById(id);
+
+        if(fundingInfo.isEmpty()) throw new NoFundingExistException();
+
+        return fundingInfo.get();
     }
 }
