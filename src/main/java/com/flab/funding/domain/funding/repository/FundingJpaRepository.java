51c706109386f6acd771480431bc5d67afb4895e
deveun <bellar9311@gmail.com>
2022-01-16 21:26:59 +0900

    seller 기능 추가 (펀딩 삭제 , 펀딩 리스트 조회, 펀딩 상세내역 조회)

--- src/main/java/com/flab/funding/domain/funding/repository/FundingJpaRepository.java
+++ src/main/java/com/flab/funding/domain/funding/repository/FundingJpaRepository.java
@@ -1,9 +1,12 @@
 package com.flab.funding.domain.funding.repository;
 
 import com.flab.funding.domain.funding.model.FundingInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FundingJpaRepository extends JpaRepository<FundingInfo, Long> {
 
+    Page<FundingInfo> findBySeller_Id(Long id, Pageable page);
 
 }
